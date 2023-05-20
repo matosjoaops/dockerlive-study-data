a2aa82d66626df6d6b9d810b7cad1ffa96c608d3
matosjoaops <matosjoaops@protonmail.com>
2023-05-20 20:36:46 +0100

    Adding new data and updating time calculations

--- scripts/parse_timestamps.py
+++ scripts/parse_timestamps.py
@@ -17,7 +17,7 @@ def delta_to_seconds(delta):
     return int(delta.total_seconds())
 
 def setup_contexts(data):
-    contexts = ["Reading Instructions", "Firefox", "Terminal", "VSCode"]
+    contexts = ["Reading Instructions", "Firefox", "Terminal", "VSCode", "File Manager", "File Editor", "Excluded"]
     for context in contexts:
         if context not in data:
             data[context] = 0
@@ -30,7 +30,8 @@ def process_task(filename):
         most_recent_timestamp = None
         most_recent_context = None
         last_timestamp = None
-        context_switches = 0
+        data["Context Switches"] = 0
+        data["Total Duration"] = 0
 
         setup_contexts(data)
 
@@ -41,19 +42,21 @@ def process_task(filename):
             if row["Context"] == "End":
                 last_timestamp = parse_timestamp(row["Timestamp"])
                 total_time = last_timestamp - first_timestamp
-                data["Total Duration"] = delta_to_seconds(total_time)
+                data["Total Duration"] += delta_to_seconds(total_time)
 
             if most_recent_timestamp is not None and most_recent_context is not None:
                 time_since_last_context_change = parse_timestamp(row["Timestamp"]) - most_recent_timestamp
                 time_in_seconds = delta_to_seconds(time_since_last_context_change)
                 data[most_recent_context] += time_in_seconds
+                if most_recent_context == "Excluded":
+                    data["Total Duration"] -= time_in_seconds
+                    data["Context Switches"] -= 1
  
             most_recent_context = row["Context"]
             most_recent_timestamp = parse_timestamp(row["Timestamp"])
-            context_switches += 1
+            data["Context Switches"] += 1
         
-        context_switches -= 1 #"End" doesn't count
-        data["Context Switches"] = context_switches
+        data["Context Switches"] -= 1 #"End" doesn't count
 
         return data
 
