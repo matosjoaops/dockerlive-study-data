9305c0da12368fa48feedebaf4fa9a9b19cc8f86
matosjoaops <matosjoaops@protonmail.com>
2023-05-22 15:21:47 +0100

    Adding preliminary data from questionnaires

--- scripts/anonymize_data.py
+++ scripts/anonymize_data.py
@@ -12,15 +12,16 @@ def anonymize_group(filename, num_assignment):
         with open(f"anon-data/{filename}", "w") as new_file:
             new_file.write(new_file_content)
 
-def anonymize_background(num_assignment):
-    with open("data/background.csv", "r") as file:
+def anonymize_questionnaire(filename, num_assignment):
+    with open(f"data/{filename}", "r") as file:
         reader = csv.DictReader(file)
-        with open("anon-data/background.csv", "w") as new_file:
+        with open(f"anon-data/{filename}", "w") as new_file:
             writer = csv.writer(new_file)
             wrote_header = False
             for row in reader:
                 row.pop("Carimbo de data/hora")
-                row.pop("Are you willing and interested to participate in this study?")
+                if "Are you willing and interested to participate in this study?" in row:
+                    row.pop("Are you willing and interested to participate in this study?")
                 row.pop("What is your name?")
                 email = row["What is your email address?"]
                 row.pop("What is your email address?")
@@ -34,11 +35,13 @@ def anonymize_background(num_assignment):
 
 num_assignment = {}
         
-with open("data/participant_num.csv", "r") as assignment_file:
+with open("data/participant_id.csv", "r") as assignment_file:
     reader = csv.DictReader(assignment_file, fieldnames=["email", "num"])
     for row in reader:
         num_assignment[row["email"]] = row["num"]
 
 anonymize_group("control.txt", num_assignment)
 anonymize_group("experimental.txt", num_assignment)
-anonymize_background(num_assignment)
+anonymize_questionnaire("background.csv", num_assignment)
+anonymize_questionnaire("control_questionnaire.csv", num_assignment)
+anonymize_questionnaire("experimental_questionnaire.csv", num_assignment)
